2203dfdc93e1a908d59b010830a97588a737461b
Samuel Sublate <samuelsublate.dev@gmail.com>
2023-09-08 00:39:39 -0300

    feat: implemented ingredient bread calculator

---
+++ calculator/normal_bread/ingredient_factor.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class IngredientFactor(Enum):
+    
+    MILK = 0.46665
+    WATER = 0.46665
+    DRY_YEAST = 0.01
+    SUGAR = 0.10
+    SALT = 0.02
+    BUTTER = 0.08
+    EGG = 1
